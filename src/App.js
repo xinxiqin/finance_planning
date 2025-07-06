36009b8c4a80138e24c1b6d59f4f8aa297db23a0
Xinxi Qin <xinxi.qin@gmail.com>
2025-07-06 10:44:22 -0400

    title change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          finance planning
+          finance planning by Xinxi Qin
         </p>
 
       </header>
